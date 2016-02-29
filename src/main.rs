487b4da8bdc79147999d59797aa9298219590ceb
Hubert Figuière <hub@figuiere.net>
2016-02-29 14:03:58 -0500

    Issue #5 - Add command line args for host and port.

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@
 /// Boxes are supposed to register themselves at regular intervals so we
 /// discard data which is too old periodically.
 
+extern crate docopt;
 extern crate env_logger;
 extern crate iron;
 #[macro_use]
@@ -19,8 +20,10 @@ extern crate log;
 extern crate mount;
 extern crate params;
 extern crate router;
+extern crate rustc_serialize;
 
 use db::Db;
+use docopt::Docopt;
 use iron::Iron;
 use mount::Mount;
 
@@ -28,8 +31,27 @@ mod db;
 mod routes;
 mod eviction;
 
+const USAGE: &'static str = "
+Usage: registration_server [-h <hostname>] [-p <port>]
+
+Options:
+    -h, --host <host>        Set local hostname.
+    -p, --port <port>        Set port to listen on for http connections.
+";
+
+
+#[derive(RustcDecodable)]
+struct Args {
+    flag_host: Option<String>,
+    flag_port: Option<u16>,
+}
+
+
 fn main() {
     env_logger::init().unwrap();
+
+    let args: Args = Docopt::new(USAGE).and_then(|d| d.decode())
+        .unwrap_or_else(|e| e.exit());
 
     Db::new();
 
@@ -38,9 +60,26 @@ fn main() {
     let mut mount = Mount::new();
     mount.mount("/", routes::create());
 
-    // TODO: add a command line flag to set host:port.
-    info!("Starting server on 0.0.0.0:4242");
-    Iron::new(mount).http("0.0.0.0:4242").unwrap();
+    let host = args.flag_host.unwrap_or("0.0.0.0".to_string());
+    let port = args.flag_port.unwrap_or(4242);
+    info!("Starting server on {}:{}", host, port);
+    Iron::new(mount).http(format!("{}:{}", host, port).as_ref() as &str)
+        .unwrap();
 }
 
 // TODO: add iron tests.
+
+#[test]
+fn options_are_good() {
+    // short form options
+    {
+        let argv = || vec!["registration_server", "-p", "1234", "-h", "foobar"];
+
+        let args: Args = Docopt::new(USAGE)
+            .and_then(|d| d.argv(argv().into_iter()).decode())
+            .unwrap();
+
+        assert_eq!(args.flag_host, Some("foobar".to_string()));
+        assert_eq!(args.flag_port, Some(1234));
+    }
+}
