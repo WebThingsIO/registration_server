09618a9c1fa568ae07df46a381c445fbf7dd83b9
Fabrice Desré <fabrice@desre.org>
2017-06-01 12:23:23 -0700

    fix launch path

--- run_from_docker.sh
+++ run_from_docker.sh
@@ -10,4 +10,4 @@ pdns_server --config-dir=$ROOT_DIR
 
 pagekite.py --isfrontend --ports=4443 --protos=https --domain=https:*.$DOMAIN:$SECRET --authdomain=$DOMAIN &
 
-./server/target/release/main --config-file=$ROOT_DIR/config.toml
+./target/release/main --config-file=$ROOT_DIR/config.toml
