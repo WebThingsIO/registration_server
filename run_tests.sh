2e1cc6db256d3388e2c7ee14574c5667ee72a679
Michael Stegeman <michael@stegeman.me>
2020-09-06 23:07:44 -0800

    Switch back to expected mysql password.

--- run_tests.sh
+++ run_tests.sh
@@ -5,10 +5,10 @@ set -e
 for db_type in mysql postgres sqlite; do
     for database in domain_db_test_domains domain_db_test_email domain_db_test_pdns domain_db_test_routes; do
         if [ "${db_type}" = "mysql" ]; then
-            db_path="mysql://root:password@127.0.0.1:3306/${database}"
-            mysql -u root -p password -h 127.0.0.1 -P 3306 -e "drop database ${database}" >/dev/null 2>&1 || true
+            db_path="mysql://root:root@127.0.0.1/${database}"
+            mysql -u root -p root -h 127.0.0.1 -P 3306 -e "drop database ${database}" >/dev/null 2>&1 || true
         elif [ "${db_type}" = "postgres" ]; then
-            db_path="postgres://postgres:password@127.0.0.1:5432/${database}"
+            db_path="postgres://postgres:password@127.0.0.1/${database}"
             PGPASSWORD=password dropdb -U postgres -h 127.0.0.1 -p 5432 "${database}" >/dev/null 2>&1 || true
         elif [ "${db_type}" = "sqlite" ]; then
             db_path="./${database}.sqlite"
